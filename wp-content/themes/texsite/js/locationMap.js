163115e8146db3ede4f9b3a3207b9d50097f98f4
Roman Brito <romanbrito1@gmail.com>
2017-08-14 09:24:47 -0500

    removed som unecessary comments from locationsMap.js

--- wp-content/themes/texsite/js/locationMap.js
+++ wp-content/themes/texsite/js/locationMap.js
@@ -16,28 +16,12 @@ var getJSON = function (url, callback) {
 
 function initMap() {
 
-    // var pos = {};
-    //
-    // navigator.geolocation.getCurrentPosition(function (position) {
-    //     console.log("lat", position.coords.latitude);
-    //     console.log("lng", position.coords.longitude);
-    //
-    //     pos = {
-    //         lat: position.coords.latitude,
-    //         lng: position.coords.longitude
-    //     };
-
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 11,
-        //center: {lat: 30.3947961, lng: -97.7490322}
-        //center: pos
+     var map = new google.maps.Map(document.getElementById('map'), {
+     // center and zoom in bounds
     });
 
     // center to bounds
     var bounds = new google.maps.LatLngBounds();
-
-    // Create an array of alphabetical characters used to label the markers.
-    //var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
     // Add some markers to the map.
     // Note: The code uses the JavaScript Array.prototype.map() method to
@@ -50,9 +34,10 @@ function initMap() {
             var loc = new google.maps.LatLng(location.coordinates);
             bounds.extend(loc);
 
+            // adding markers
             return new google.maps.Marker({
                 position: location.coordinates,
-                //label: labels[i % labels.length]
+                //labels
                 label: location.label,
             });
         });
@@ -60,13 +45,10 @@ function initMap() {
         var markerCluster = new MarkerClusterer(map, markers,
             {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
 
-        // Add links to markers to open in google maps
+        // Add links to markers to open in google maps for directions
         var gMapsClick = data.locations.map(function (location, i) {
             return markers[i].addListener('click', function () {
                 window.open('https://www.google.com/maps/dir/?api=1&destination=' + location.coordinates.lat + ',' + location.coordinates.lng, '_blank');
-
-                //window.location.href = 'https://www.google.com/maps/search/?api=1&place=Texadelphia&query=' + location.lat + ',' + location.lng;
-                //window.location.href = 'https://www.google.com/maps/search/?api=1&query_place_id=ChIJOyLf3YPMRIYRPnExMnxcU04'
             })
         });
 
@@ -74,12 +56,5 @@ function initMap() {
         map.fitBounds(bounds);
         map.panToBounds(bounds);
 
-        // var infoWindow = new google.maps.InfoWindow;
-        // infoWindow.setPosition(pos);
-        // infoWindow.setContent('Your Location');
-        // infoWindow.open(map);
-
     });
-
-//    });
 }
