6d9c31f42849ae84a4b5f1e3c6a57b0e34fe8001
Roman Brito <romanbrito1@gmail.com>
2017-08-17 22:09:05 -0500

    removed site info form footer

--- wp-content/themes/texsite/footer.php
+++ wp-content/themes/texsite/footer.php
@@ -31,17 +31,6 @@
         </nav>
 
 
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'texsite' ) ); ?>"><?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'texsite' ), 'WordPress' );
-			?></a>
-			<span class="sep"> | </span>
-			<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'texsite' ), 'texsite', '<a href="http://romanbrito.pro">roman</a>' );
-			?>
-		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
