ee0f2bdcc25e82a61c2b10db0b743eb70da7e15d
Roman Brito <romanbrito1@gmail.com>
2017-08-24 16:21:57 -0500

    switch location info with buttons

--- wp-content/themes/texsite/js/searchLocation.js
+++ wp-content/themes/texsite/js/searchLocation.js
@@ -1,4 +1,4 @@
-(function( $ ) {
+(function ($) {
     /**
      * Search script
      */
@@ -17,30 +17,33 @@
                 if ((val.name.search(myExp) != -1) ||
                     (val.address.search(myExp) != -1) ||
                     (val.zip.search(myExp) != -1) ||
-                    (val.state.search(myExp) != -1)||
+                    (val.state.search(myExp) != -1) ||
                     (val.city.search(myExp) != -1)) {
                     output += '<li>';
                     output += '<div class="row main-location">';
+
+                    output += '<div class="location-info col-lg-6">';
+                    output += '<h4>' + val.name + '</h4>';
+                    output += '<p>' + val.address + '</p>';
+                    output += '<p>' + val.city + ', ';
+                    output += val.state + ' ';
+                    output += val.zip + '</p>';
+                    output += '<a href="tel:' + val.phone + '">' + 'T. ' + val.phone + '</a>';
+                    output += '<p>' + val.hours1 + '</p>';
+                    output += '<p>' + val.hours2 + '</p>';
+                    output += '<p>' + val.hours3 + '</p>';
+                    output += '</div>';
+
                     output += '<div class="location-buttons col-lg-6">';
                     output += '<div class="row">';
                     output += '<a class="btn btn-default col-lg-6 col-md-6 col-sm-6 col-xs-6" role="button" data-toggle="modal" data-target="' + '#' + val.label + 'menuModal"> Menu</a>';
                     output += '<a class="btn btn-default col-lg-6 col-md-6 col-sm-6 col-xs-6" role="button" data-toggle="modal" data-target="' + '#' + val.label + 'catModal"> Catering Menu</a>';
                     output += '</div>';
                     output += '<div class="row">';
-                    output += '<a class="btn btn-danger" href="https://www.google.com/maps/dir/?api=1&destination='+ val.coordinates.lat + ',' + val.coordinates.lng + '" target="_blank" role="button"> Directions</a>';
+                    output += '<a class="btn btn-danger" href="https://www.google.com/maps/dir/?api=1&destination=' + val.coordinates.lat + ',' + val.coordinates.lng + '" target="_blank" role="button"> Directions</a>';
                     output += '</div>';
                     output += '</div>';
-                    output += '<div class="location-info col-lg-6">';
-                    output += '<h4>' + val.name + '</h4>';
-                    output += '<p>' + val.address + '</p>';
-                    output += '<p>' + val.city + ', ';
-                    output +=  val.state + ' ';
-                    output +=  val.zip + '</p>';
-                    output +=  '<a href="tel:' + val.phone + '">' + 'T. ' + val.phone + '</a>';
-                    output += '<p>' + val.hours1 + '</p>';
-                    output += '<p>' + val.hours2 + '</p>';
-                    output += '<p>' + val.hours3 + '</p>';
-                    output += '</div>';
+
                     output += '</div>';
                     output += '</li>';
                 }
@@ -56,26 +59,29 @@
             $.each(data.locations, function (key, val) {
                 output += '<li>';
                 output += '<div class="row main-location">';
+
+                output += '<div class="location-info col-lg-6">';
+                output += '<h4>' + val.name + '</h4>';
+                output += '<p>' + val.address + '</p>';
+                output += '<p>' + val.city + ', ';
+                output += val.state + ' ';
+                output += val.zip + '</p>';
+                output += '<a href="tel:' + val.phone + '">' + 'T. ' + val.phone + '</a>';
+                output += '<p>' + val.hours1 + '</p>';
+                output += '<p>' + val.hours2 + '</p>';
+                output += '<p>' + val.hours3 + '</p>';
+                output += '</div>';
+
                 output += '<div class="location-buttons col-lg-6">';
                 output += '<div class="row">';
                 output += '<a class="btn btn-default col-lg-6 col-md-6 col-sm-6 col-xs-6" role="button" data-toggle="modal" data-target="' + '#' + val.label + 'menuModal"> Menu</a>';
                 output += '<a class="btn btn-default col-lg-6 col-md-6 col-sm-6 col-xs-6" role="button" data-toggle="modal" data-target="' + '#' + val.label + 'catModal"> Catering Menu</a>';
                 output += '</div>';
                 output += '<div class="row">';
-                output += '<a class="btn btn-danger" href="https://www.google.com/maps/dir/?api=1&destination='+ val.coordinates.lat + ',' + val.coordinates.lng + '" target="_blank" role="button"> Directions</a>';
+                output += '<a class="btn btn-danger" href="https://www.google.com/maps/dir/?api=1&destination=' + val.coordinates.lat + ',' + val.coordinates.lng + '" target="_blank" role="button"> Directions</a>';
                 output += '</div>';
                 output += '</div>';
-                output += '<div class="location-info col-lg-6">';
-                output += '<h4>' + val.name + '</h4>';
-                output += '<p>' + val.address + '</p>';
-                output += '<p>' + val.city + ', ';
-                output +=  val.state + ' ';
-                output +=  val.zip + '</p>';
-                output +=  '<a href="tel:' + val.phone + '">' + 'T. ' + val.phone + '</a>';
-                output += '<p>' + val.hours1 + '</p>';
-                output += '<p>' + val.hours2 + '</p>';
-                output += '<p>' + val.hours3 + '</p>';
-                output += '</div>';
+
                 output += '</div>';
                 output += '</li>';
             });
@@ -85,4 +91,4 @@
     }
 
 
-})( jQuery );
+})(jQuery);
