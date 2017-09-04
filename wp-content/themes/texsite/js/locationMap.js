8ba5a9bfb878ed6e8baafd00117862c1f6497df6
Roman Brito <romanbrito1@gmail.com>
2017-09-04 08:52:32 -0500

    removed ie condition, as it now works without it....

--- wp-content/themes/texsite/js/locationMap.js
+++ wp-content/themes/texsite/js/locationMap.js
@@ -103,7 +103,7 @@ function initMap() {
     $.getJSON('../wp-content/themes/texsite/json/locations.json', function (data) {
 
         console.log(detectIE());
-        if (detectIE() === false) {
+        //if (detectIE() === false) {
 
             var markers = data.locations.map(function (location, i) {
                 // center to bounds
@@ -118,22 +118,22 @@ function initMap() {
                 });
             });
 
-        } else {
-            var markers = JSON.parse(data).locations.map(function (location, i) {
-                // center to bounds
-                var loc = new google.maps.LatLng(location.coordinates);
-                bounds.extend(loc);
+        // } else {
+        //     var markers = JSON.parse(data).locations.map(function (location, i) {
+        //         // center to bounds
+        //         var loc = new google.maps.LatLng(location.coordinates);
+        //         bounds.extend(loc);
+        //
+        //         // adding markers
+        //         return new google.maps.Marker({
+        //             position: location.coordinates,
+        //             //labels
+        //             label: location.label,
+        //         });
+        //     });
+        // }
 
-                // adding markers
-                return new google.maps.Marker({
-                    position: location.coordinates,
-                    //labels
-                    label: location.label,
-                });
-            });
-        }
-
-        if (detectIE() === false) {
+        // if (detectIE() === false) {
             // Add a marker clusterer to manage the markers.
             var markerCluster = new MarkerClusterer(map, markers,
                 {
@@ -148,21 +148,21 @@ function initMap() {
                 });
             });
 
-        } else {
-            // Add a marker clusterer to manage the markers.
-            var markerCluster = new MarkerClusterer(map, markers,
-                {
-                    gridSize: 15,
-                    imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m',
-                });
-
-            // Add links to markers to open in google maps for directions
-            var gMapsClick = JSON.parse(data).locations.map(function (location, i) {
-                return markers[i].addListener('click', function () {
-                    window.open('https://www.google.com/maps/dir/?api=1&destination=' + location.coordinates.lat + ',' + location.coordinates.lng, '_blank');
-                });
-            });
-        }
+        // } else {
+        //     // Add a marker clusterer to manage the markers.
+        //     var markerCluster = new MarkerClusterer(map, markers,
+        //         {
+        //             gridSize: 15,
+        //             imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m',
+        //         });
+        //
+        //     // Add links to markers to open in google maps for directions
+        //     var gMapsClick = JSON.parse(data).locations.map(function (location, i) {
+        //         return markers[i].addListener('click', function () {
+        //             window.open('https://www.google.com/maps/dir/?api=1&destination=' + location.coordinates.lat + ',' + location.coordinates.lng, '_blank');
+        //         });
+        //     });
+        // }
 
         // center to bounds
         map.fitBounds(bounds);
