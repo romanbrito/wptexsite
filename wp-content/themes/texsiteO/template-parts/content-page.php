306a715f731341abb790fa0d3b2d4a2612e83074
Roman Brito <romanbrito1@gmail.com>
2017-10-27 05:51:16 -0500

    some format to forms contact

--- wp-content/themes/texsiteO/template-parts/content-page.php
+++ wp-content/themes/texsiteO/template-parts/content-page.php
@@ -13,7 +13,7 @@
     <header class="entry-header">
         <?php
         // do not show title on locations and menu page, About us and Franchising
-        if (!is_front_page() and !is_page('locations-menu') and !is_page('about-us') and !is_page('we-cater') and !is_page('franchising') and !is_page('texnation-loyalty-program')):
+        if (!is_front_page() and !is_page('locations-menu') and !is_page('about-us') and !is_page('we-cater') and !is_page('franchising') and !is_page('texnation-loyalty-program') and !is_page('contact-us')):
         the_title('<h1 class="entry-title">', '</h1>');
         endif;
 
